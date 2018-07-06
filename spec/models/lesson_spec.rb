1960f2de387a7cb46e0ee76c3425e206475d88d7
Showner <dev@showner.fr>
2018-07-06 07:49:59 +0200

    require faker in Lesson fatory

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -9,6 +9,8 @@
 #  updated_at  :datetime         not null
 #
 
+require 'faker'
+
 RSpec.describe Lesson, type: :model do
   it { should validate_presence_of(:title) }
   it { should validate_length_of(:title).is_at_most(50) }
